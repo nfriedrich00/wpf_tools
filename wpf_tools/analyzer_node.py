6d95a3d554da31ebafd3749df658654b7d724ca2
Nils Friedrich <nils-jonathan.friedrich@student.tu-freiberg.de>
2024-06-21 15:31:29 +0200

    pylance

--- wpf_tools/analyzer_node.py
+++ wpf_tools/analyzer_node.py
@@ -10,7 +10,10 @@ from geometry_msgs.msg import Point, Pose, Quaternion, PoseStamped
 from std_msgs.msg import Header
 from rclpy.node import Node
 
-from .wpf_utils import *
+from wpf_tools.wpf_utils import (float_to_time, convert_dict_to_list,
+                                 get_distance_through_poses, convert_list_type,
+                                 get_max_distance, get_max_dist_simpl,
+                                 get_avg_distance, get_avg_dist_simpl)
 
 
 class AnalyzerNode(Node):
@@ -69,36 +72,34 @@ class AnalyzerNode(Node):
             localization_data = yaml.safe_load(localization_file)
         self.get_logger().info("Data loaded successfully.")
 
-
         # start time when actually starting to move
         start_time = goal_checker_data['start moving']
         finish_time = goal_checker_data['goal succeeded']
 
         traveled_time = finish_time - start_time
 
-
         # Get AVERAGE SPEED
         path_poses = []
         path_key = list(path_data.keys())[0]
         path_stamp = float_to_time(path_key)
-        path_header = Header(stamp = path_stamp)
+        path_header = Header(stamp=path_stamp)
         for waypoint in path_data[path_key]['waypoints']:
-            position = Point(x = waypoint['x'],
-                            y = waypoint['y'],
-                            z = waypoint['z'])
-            orientation = Quaternion(x = waypoint['q0'], y = waypoint['q1'],
-                            z = waypoint['q2'], w = waypoint['q3'])
-            pose = Pose(position = position, orientation = orientation)
-            path_poses.append(PoseStamped(header = path_header,
-                                          pose = pose))
+            position = Point(x=waypoint['x'],
+                             y=waypoint['y'],
+                             z=waypoint['z'])
+            orientation = Quaternion(x=waypoint['q0'], y=waypoint['q1'],
+                                     z=waypoint['q2'], w=waypoint['q3'])
+            pose = Pose(position=position, orientation=orientation)
+            path_poses.append(PoseStamped(header=path_header,
+                                          pose=pose))
 
-        ground_truth_poses = convert_dict_to_list(start_time = start_time,
-                                                  end_time = finish_time,
-                                                  dic = ground_truth_data)
+        ground_truth_poses = convert_dict_to_list(start_time=start_time,
+                                                  end_time=finish_time,
+                                                  dic=ground_truth_data)
 
-        localization_poses = convert_dict_to_list(start_time = start_time,
-                                                  end_time = finish_time,
-                                                  dic = localization_data)
+        localization_poses = convert_dict_to_list(start_time=start_time,
+                                                  end_time=finish_time,
+                                                  dic=localization_data)
 
         path_distance = get_distance_through_poses(path_poses)
         ground_truth_distance = get_distance_through_poses(ground_truth_poses)
@@ -108,24 +109,21 @@ class AnalyzerNode(Node):
         average_speed_real = ground_truth_distance / traveled_time
         average_speed_assumed = localization_distance / traveled_time
 
-        average_speed = {'average_speed_effective' : average_speed_effective,
-                         'average_speed_real' : average_speed_real,
-                         'average_speed_assumed' : average_speed_assumed}
-        
-        distance_data = {'path' : path_distance,
-                         'ground_truth' : ground_truth_distance,
+        average_speed = {'average_speed_effective': average_speed_effective,
+                         'average_speed_real': average_speed_real,
+                         'average_speed_assumed': average_speed_assumed}
+
+        distance_data = {'path': path_distance,
+                         'ground_truth': ground_truth_distance,
                          'localization': localization_distance}
 
-        #debug
+        # debug
         print(f'start_time: {start_time}')
         print(f'finish_time: {finish_time}')
         print(f'path distance: {path_distance}')
         print(f'ground truth distance: {ground_truth_distance}')
         print(f'localization distance: {localization_distance}')
         print(f'average_speed_effective: {average_speed["average_speed_effective"]}')
-        print(f'average_speed_real: {average_speed["average_speed_real"]}')
-        print(f'average_speed_assumed: {average_speed["average_speed_assumed"]}')
-
 
         # GET ERROR
         localization_points = convert_list_type(localization_poses, Point())
@@ -133,41 +131,41 @@ class AnalyzerNode(Node):
         path_points = convert_list_type(path_poses, Point())
 
         # MAX
-        #max_error_loc = get_max_distance(self,
+        # max_error_loc = get_max_distance(self,
         max_error_loc = get_max_distance(self,
-                                         inaccurate_poses = localization_poses,
-                                         reference_poses = ground_truth_poses)
-        max_error_loc_simpl = get_max_dist_simpl(points = localization_points,
-                                                 reference_points = ground_truth_points)
-        avg_error_loc = get_avg_distance(inaccurate_poses = localization_poses,
-                                         reference_poses = ground_truth_poses)
-        avg_error_loc_simpl = get_avg_dist_simpl(inaccurate_points = localization_points,
-                                                 reference_points = ground_truth_points)
-        
-        max_error_real = get_max_dist_simpl(points = ground_truth_points,
-                                            reference_points = path_points)
-        avg_error_real = get_avg_dist_simpl(inaccurate_points = ground_truth_points,
-                                            reference_points = path_points)
-        
-        max_error_contr = get_max_dist_simpl(points = localization_points,
-                                             reference_points = path_points)
-        avg_error_contr = get_avg_dist_simpl(inaccurate_points = localization_points,
-                                             reference_points = path_points)
+                                         inaccurate_poses=localization_poses,
+                                         reference_poses=ground_truth_poses)
+        max_error_loc_simpl = get_max_dist_simpl(localization_points,
+                                                 ground_truth_points)
+        avg_error_loc = get_avg_distance(localization_poses,
+                                         ground_truth_poses)
+        avg_error_loc_simpl = get_avg_dist_simpl(localization_points,
+                                                 ground_truth_points)
 
-        localization_errors = {'max' : float(max_error_loc),
-                               'max simplified' : float(max_error_loc_simpl),
-                               'average' : float(avg_error_loc),
-                               'average simplified' : float(avg_error_loc_simpl)}
-        
-        real_errors = {'max' : float(max_error_real),
-                       'average' : float(avg_error_real)}
-        
-        controller_errors = {'max' : float(max_error_contr),
-                             'average' : float(avg_error_contr)}
+        max_error_real = get_max_dist_simpl(points=ground_truth_points,
+                                            reference_points=path_points)
+        avg_error_real = get_avg_dist_simpl(ground_truth_points,
+                                            path_points)
 
-        error_data = {'localization' : localization_errors,
-                      'real (path - ground truth)' : real_errors,
-                      'controller (path - localization)' : controller_errors}
+        max_error_contr = get_max_dist_simpl(points=localization_points,
+                                             reference_points=path_points)
+        avg_error_contr = get_avg_dist_simpl(localization_points,
+                                             path_points)
+
+        localization_errors = {'max': float(max_error_loc),
+                               'max simplified': float(max_error_loc_simpl),
+                               'average': float(avg_error_loc),
+                               'average simplified': float(avg_error_loc_simpl)}
+
+        real_errors = {'max': float(max_error_real),
+                       'average': float(avg_error_real)}
+
+        controller_errors = {'max': float(max_error_contr),
+                             'average': float(avg_error_contr)}
+
+        error_data = {'localization': localization_errors,
+                      'real (path - ground truth)': real_errors,
+                      'controller (path - localization)': controller_errors}
 
         # debug
         print(f'max loc error0: {max_error_loc}')
@@ -177,11 +175,10 @@ class AnalyzerNode(Node):
         print(f'max nav error:  {max_error_real}')
         print(f'avg nav error:  {avg_error_real}')
 
-
         # save it
-        results = {'errors' : error_data,
-                   'speed' : average_speed,
-                   'distance' : distance_data}
+        results = {'errors': error_data,
+                   'speed': average_speed,
+                   'distance': distance_data}
         self.log_results(results)
 
     def log_results(self, results):
@@ -189,10 +186,8 @@ class AnalyzerNode(Node):
         '''
         self.get_logger().info('Saving data ...')
         logfile = self.logs_path + '/results.yaml'
-        with open(logfile, 'w', encoding = 'utf-8') as file:
+        with open(logfile, 'w', encoding='utf-8') as file:
             yaml.dump(results, file, default_flow_style=False)
-
-
 
 
 def main(args=None):
@@ -204,5 +199,7 @@ def main(args=None):
     rclpy.spin(analyzer_node)
     analyzer_node.destroy_node()
     rclpy.shutdown()
+
+
 if __name__ == '__main__':
     main()
