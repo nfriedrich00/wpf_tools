341810612af9de894e800a89a7bc28da470cea38
Georg Jäger <georg.jaeger@informatik.tu-freiberg.de>
2024-09-30 11:54:40 +0200

    enable running same configuration several times

--- start_experiments.sh
+++ start_experiments.sh
@@ -1,25 +1,23 @@
 #! /bin/bash
 
-max_runtime=300
-config_filepath=~/Documents/ros2/wpf_ws/install/wpf_tools/share/wpf_tools/config/waypoint_follower_config.yaml
+# run experiments for several times to get the average results
 
-# get the path of the script
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+loops=10
 
-# store current path
-current_path=$(pwd)
-
-# set handler for SIGINT and kill all tmux sessions starting with $SESSION
-trap 'echo "SIGINT detected in start_experiments.sh! Exiting..."; cd "$current_path"; sleep 10; exit 1' SIGINT
-
-# switch to the script path
-cd $SCRIPTPATH
-
-# execute experiments
-while true; do
-    # call start_single_experiment.sh with the same arguments
-    ./start_single_experiment.sh -c $config_filepath -t $max_runtime
+# parse arguments with getopts
+while getopts "k:" opt; do
+  case $opt in
+    k)
+      echo "k = $OPTARG"
+      loops=$OPTARG
+      ;;
+  esac
 done
 
-# switch back to the original path
-cd $current_path
+# run experiments
+for ((i=1; i<=$loops; i++))
+do
+  echo "Run experiment $i"
+  # parse all unread arguments to start_single_experiment.sh
+  ./start_single_experiment.sh $@
+done
