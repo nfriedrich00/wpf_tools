49475a7f302ba477bd1fd6f828af67ffdb42ee39
Georg Jäger <georg.jaeger@informatik.tu-freiberg.de>
2024-09-30 12:04:11 +0200

    update to filter invalid cmd option

--- start_experiments.sh
+++ start_experiments.sh
@@ -2,22 +2,24 @@
 
 # run experiments for several times to get the average results
 
-loops=10
-
-# parse arguments with getopts
+# parse arguments, here we only need -k to specify the number of loops, other arguments will be stored in other_args
+loops=1
+other_args=""
 while getopts "k:" opt; do
   case $opt in
-    k)
-      echo "k = $OPTARG"
-      loops=$OPTARG
-      ;;
+    k) loops=$OPTARG
+    continue
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
   esac
+  other_args="$other_args $OPTARG"
 done
 
 # run experiments
 for ((i=1; i<=$loops; i++))
 do
   echo "Run experiment $i"
-  # parse all unread arguments to start_single_experiment.sh
+  # parse all arguments (execpt k) to start_single_experiment.sh
   ./start_single_experiment.sh $@
 done
