3a3a67e63bdb6384331a7c66486ef88d46730141
Nils Friedrich <nils-jonathan.friedrich@student.tu-freiberg.de>
2025-02-20 11:21:39 +0100

    revise analysis to match new logs structure and use parameters to limit analysis time interval

--- wpf_tools/analyzer_node.py
+++ wpf_tools/analyzer_node.py
@@ -24,59 +24,151 @@ class AnalyzerNode(Node):
     def __init__(self):
         super().__init__('analyzer_node')
 
-        # first get session identifier to select files to analyze
+        default_logs_parent_dir = os.path.expanduser("~") + "/Documents/wpf/logs"
 
-        # the following directory contains exactly one file for each session
-        self.logs_path = os.path.expanduser("~") + "/Documents/wpf/logs"
-        self.results_path = os.path.expanduser("~") + "/Documents/wpf/results.yaml"
+        # temp debugging:
+        temp_debugging_path = "/home/nfriedrich/Documents/docker/jazzy_vnc/Documents/wpf/logs"
+        default_logs_parent_dir = temp_debugging_path
+        # / temp debugging
 
-        self.declare_parameter('logs_path', self.logs_path)
-        if self.get_parameter('logs_path').value is not None:
-            self.logs_path = self.get_parameter('logs_path').value
+        self.declare_parameter('logs_directory', '')
+        self.logs_dir = self.get_parameter('logs_directory').value
+        if self.logs_dir:
+            # True if parameter is set
+            self.get_logger().info(f'Logs directory provided: {self.logs_dir}')
+        else:
+            # Parameter not set, using default value
+            #default_logs_dir = ... # todo: move here
+            self.get_logger().info(f'No logs directory provided. Looking for logs in default directory {default_logs_parent_dir}.')
+            if not os.path.exists(default_logs_parent_dir):
+                self.get_logger().error(f'Default logs directory {default_logs_parent_dir} does not exist. Aborting.')
+                raise FileNotFoundError
+            list_of_log_dirs = [f for f in glob.glob(default_logs_parent_dir + '/*') if os.path.basename(f).isdigit()]
+            if not list_of_log_dirs:
+                self.get_logger().error(f'No logs found in default directory {default_logs_parent_dir}. Aborting.')
+                raise FileNotFoundError
+            path_of_newest_log = max(
+                list_of_log_dirs,
+                key=lambda x: int(os.path.basename(x)))
+            self.logs_dir = path_of_newest_log
+            self.get_logger().info(f'Using logs from latest session: {self.logs_dir}.')
+
+
+        self.session_id = pathlibPath(self.logs_dir).stem
+
+        self.declare_parameter('overwrite_results', True)
+        self.overwrite_results = self.get_parameter('overwrite_results').value
+        """
+        Parameter to decide if an existing results file should be overwritten. Otherwise, a new file with a number suffix is created.
+        """
+
+        self.declare_parameter('start_time', 0.0)
+        self.start_time = self.get_parameter('start_time').value
+        """
+        Parameter to limit the analysis to a specific time interval.
+        The analysis will only consider data with a timestamp greater or equal to this value.
+        Set to 0.0 to disable.
+        """
+
+        self.declare_parameter('end_time', 0.0)
+        self.end_time = self.get_parameter('end_time').value
+        """
+        Parameter to limit the analysis to a specific time interval.
+        The analysis will only consider data with a timestamp smaller or equal to this value.
+        Set to 0.0 to disable.
+        """
+
+        if self.end_time < self.start_time:
+            self.get_logger().error('End time is smaller than start time. Aborting.')
+            raise ValueError
         
-        self.declare_parameter('results_path', self.results_path)
-        if self.get_parameter('results_path').value is not None:
-            self.results_path = self.get_parameter('results_path').value
-
-        goal_checker_dir = self.logs_path + "/goal_checker"
-
-        list_of_goal_checker_log_files = glob.glob(goal_checker_dir + '/*')
-        path_of_newest_file = max(
-            list_of_goal_checker_log_files,
-            key=os.path.getctime)
-        name_of_newest_file = pathlibPath(path_of_newest_file).stem
-
-        self.declare_parameter('session_id', name_of_newest_file)
-        self.session_id = self.get_parameter('session_id').value
-
-        self.analyze_data()
+        self.analyze_data() #todo: change this so we can call the function with session_id as an arguments to call it for multiple sessions
         raise SystemExit
 
     def analyze_data(self):
         ''' Start data analysis. Load log files and calculate average
         speed, error and traveled distance.
         '''
+        logs_dir = self.logs_dir
+        session_id = self.session_id
+        self.get_logger().info('Start looking for data ...')
+
+        # Check if results file already exists
+        # todo: maybe move to relevant part of code
+        self.results_path = logs_dir + '/results.yaml'
+        if os.path.exists(self.results_path) and not self.overwrite_results:
+            number = 1
+            while os.path.exists(logs_dir + f'/results_{number}.yaml'):
+                number += 1
+            self.results_path = logs_dir + '/' + session_id + f'/results_{number}.yaml'
+
+        print(f"Results path: {self.results_path}")
+
+
         self.get_logger().info('Start loading data ...')
-        with open(self.logs_path + '/position/'
-                  + self.session_id + '/path.yaml', 'r',
-                  encoding='utf-8') as path_file:
-            path_data = yaml.safe_load(path_file)
 
-        with open(self.logs_path + '/goal_checker/'
-                  + self.session_id + '.yaml', 'r',
-                  encoding='utf-8') as goal_checker_file:
-            goal_checker_data = yaml.safe_load(goal_checker_file)
+        position_logs = logs_dir + '/ground_truth.yaml'
+        if not os.path.exists(position_logs):
+            self.get_logger().error(f'No position logs found for session {session_id}. Aborting.')
+            raise FileNotFoundError
+        else:
+            self.get_logger().info(f"Position logs found.")
+            with open(logs_dir + '/ground_truth.yaml', 'r',
+                    encoding='utf-8') as position_file:
+                pos_data = yaml.safe_load(position_file)
+            # skip loading this long ass file while debugging
 
-        with open(self.logs_path + '/position/'
-                  + self.session_id + '/ground_truth.yaml', 'r',
-                  encoding='utf-8') as ground_truth_file:
-            pos_data = yaml.safe_load(ground_truth_file)
+        localization_logs = logs_dir + '/localization.yaml'
+        if not os.path.exists(localization_logs):
+            self.get_logger().info(f'No localization logs found for session {session_id}.')
+            loc_data = {}
+        else:
+            self.get_logger().info(f"Localization logs found.")
+            with open(self.logs_dir + '/localization.yaml', 'r',
+                    encoding='utf-8') as localization_file:
+                loc_data = yaml.safe_load(localization_file)
 
-        with open(self.logs_path + '/position/'
-                  + self.session_id + '/localization.yaml', 'r',
-                  encoding='utf-8') as localization_file:
-            loc_data = yaml.safe_load(localization_file)
+        path_logs = logs_dir + '/path.yaml'
+        if not os.path.exists(path_logs):
+            self.get_logger().info(f'No path logs found for session {session_id}.')
+            path_data = {}
+        else:
+            self.get_logger().info(f"Path logs found.")
+            with open(logs_dir + '/path.yaml', 'r',
+                    encoding='utf-8') as path_file:
+                path_data = yaml.safe_load(path_file)
+
+        # todo: evaluate role of the goal checker
+        goal_checker_logs = logs_dir + '/goal_checker.yaml'
+        if not os.path.exists(goal_checker_logs):
+            self.get_logger().info(f'No goal checker logs found for session {session_id}.')
+            goal_checker_data = {}
+        else:
+            self.get_logger().info(f"Goal checker logs found.")
+            with open(logs_dir + '/goal_checker.yaml', 'r',
+                    encoding='utf-8') as goal_checker_file:
+                goal_checker_data = yaml.safe_load(goal_checker_file)
+
+
         self.get_logger().info("Data loaded successfully.")
+
+        # if start_time or end_time is set, use it to limit the analysis
+        if not self.start_time and not self.end_time:
+            self.start_time = 0.0
+            # self.end_time should be the last timestamp in the logs
+            # timestamp means: we don't want to use the first key, we use key 'time'
+            self.end_time = max([float(pos_data[key]['time']) for key in pos_data])
+        else:
+            self.get_logger().info('Limiting analysis to time interval' +
+                                   f' [{self.start_time}, {self.end_time}].')
+            
+        # We use the timestampe at the 'time' key and not the first key,
+        # because the first key is the logging time and not the time of the measurement.
+        pos_data = {k: v for k, v in pos_data.items()
+                    if self.start_time <= float(v['time']) <= self.end_time}
+        loc_data = {k: v for k, v in loc_data.items()
+                    if self.start_time <= float(v['time']) <= self.end_time}
+
 
         self.get_logger().info("Converting data to numpy array")
         pos_points = np.array([[pos_data[key]['position']['x'],
@@ -89,59 +181,30 @@ class AnalyzerNode(Node):
                                 loc_data[key]['position']['z'],
                                 loc_data[key]['time'],
                                 key] for key in loc_data])
-        
-        loc_points = filter_points(loc_points)
-        pos_points = filter_points(pos_points)
-
-
-        # start time when actually starting to move, if timestamp provided.
-        start_time = goal_checker_data.get('start moving', 0.0)
-
-        # finish time is goal reached timestamp, if goal was reached.
-        # else, use last timestamp in logs. Should be the same. Unless:
-        # If the goal was reached but the time was not recorded,
-        # the last timestamp in the logs may be significantly larger.
-        # -> traveled_time higher -> avg_speed slower.
-        goal_timestamp_key = 'goal succeeded'
-        if goal_timestamp_key in goal_checker_data:
-            finish_time = goal_checker_data[goal_timestamp_key]
-        else:
-            pos_timestamps = pos_points[:, -1]
-            loc_timestamps = loc_points[:, -1]
-            last_pos_time = np.max(pos_timestamps) \
-                if pos_timestamps.size > 0 else 0.0
-            last_loc_time = np.max(loc_timestamps) \
-                if loc_timestamps.size > 0 else 0.0
-            #last_pos_time = max(pos_points.keys(), default=0.0)
-            #last_loc_time = max(loc_points.keys(), default=0.0)
-            finish_time = max(last_pos_time, last_loc_time)
-
-        traveled_time = finish_time - start_time
-        if traveled_time <= 0.0:
-            error_message = 'Traveled time is zero.' + \
-            'This is probably due to incomplete or corrupted logs. Aborting.'
-            self.get_logger().error(error_message)
-            raise ValueError
-
-        
-        # restrict to points within relevant time interval (ignore measurements when standing still)
-        # todo add threshould: find timestamp, when robot pos enters relevant part of path and update mask limits
-        pos_times = pos_points[:, 3]
-        pos_mask = (pos_times >= start_time) & (pos_times <= finish_time)
-        pos_points = pos_points[pos_mask]
-
-        loc_times = loc_points[:, 3]
-        loc_mask = (loc_times >= start_time) & (loc_times <= finish_time)
-        loc_points = loc_points[loc_mask]
-
-
 
         path_key = list(path_data.keys())[0]
         path_points = np.array([[wp['x'],
                                  wp['y'],
                                  wp['z'],
                                  0.0] for wp in path_data[path_key]['waypoints']])
-        self.get_logger().info("Conversion done")
+        self.get_logger().info("Conversion done.")
+
+
+        self.get_logger().info("Start analyzing data ...")
+
+        # We do no longer need to filter the points, BUT
+        # todo: we need to check the plausibility of the data
+        #loc_points = filter_points(loc_points)
+        #pos_points = filter_points(pos_points)
+
+        # These time limits do not necessarily match with the actual robot movement.
+        # We would need to check the position data for that
+        # Instead, make sure to set the limits to not include the robot standing still,
+        # if the average speed is important.
+        time_interval = self.end_time - self.start_time
+        """
+        The length of the time interval of the analyzed data is used to calculate the average speed.
+        """
 
 
         # Get errors
@@ -245,7 +308,7 @@ class AnalyzerNode(Node):
         
         # Get speed
         self.get_logger().info("Getting speed ...")
-        avg_speed = ground_truth_distance / traveled_time
+        avg_speed = ground_truth_distance / time_interval
         self.get_logger().info(f"Got avg speed: {avg_speed}")
         speed_data = {'average' : float(avg_speed)}
 
