8a1c8ea531e89968d18ed5a36aadfda314be66f7
Nils Friedrich <nils-jonathan.friedrich@student.tu-freiberg.de>
2025-02-22 19:58:51 +0100

    make sure the backup directory always exists

--- save_results.sh
+++ save_results.sh
@@ -5,14 +5,14 @@ WATCHED_FILE="/home/ubuntu/Documents/icra/logs/results.yaml"
 LOGS_DIR="/home/ubuntu/Documents/icra/logs"
 BACKUP_DIR="/home/ubuntu/Documents/icra/backup"
 
-if [ ! -d "$BACKUP_DIR" ]; then
-    mkdir -p "$BACKUP_DIR"
-fi
 
 while true; do
     if [ -f "$WATCHED_FILE" ]; then
         TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
         TARGET_DIR="$BACKUP_DIR/$TIMESTAMP"
+        if [ ! -d "$BACKUP_DIR" ]; then
+            mkdir -p "$BACKUP_DIR"
+        fi
         mv "$LOGS_DIR" "$TARGET_DIR"
         echo "Moved logs to $TARGET_DIR"
     fi
