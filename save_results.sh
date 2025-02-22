f2ec770c3d3b4b7369792df9ab1b5ffe758856d2
Nils Friedrich <nils-jonathan.friedrich@student.tu-freiberg.de>
2025-02-22 19:31:23 +0100

    bug fix

--- save_results.sh
+++ save_results.sh
@@ -5,12 +5,16 @@ WATCHED_FILE="/home/ubuntu/Documents/icra/logs/results.yaml"
 LOGS_DIR="/home/ubuntu/Documents/icra/logs"
 BACKUP_DIR="/home/ubuntu/Documents/icra/backup"
 
+if [ ! -d "$BACKUP_DIR" ]; then
+    mkdir -p "$BACKUP_DIR"
+fi
+
 while true; do
     if [ -f "$WATCHED_FILE" ]; then
         TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
         TARGET_DIR="$BACKUP_DIR/$TIMESTAMP"
         mv "$LOGS_DIR" "$TARGET_DIR"
-        echo "Moved logs to $TARGET_DIR" >> /home/ubuntu/Documents/icra/log_monitor.log
+        echo "Moved logs to $TARGET_DIR"
     fi
     sleep 1
 done
